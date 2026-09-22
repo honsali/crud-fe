d807faea8fdec426d01a01b4c151d7630b34ac49
honsali <honsali@gmail.com>
2026-09-22 15:15:33 +0000

    refactor: extract generic record consultation hook

--- src/modules/rh/departement/consulter/element/EtatDepartement.tsx
+++ src/modules/rh/departement/consulter/element/EtatDepartement.tsx
@@ -1,8 +1,13 @@
-import { FormulaireConsultation, Texte } from 'waxant';
-import useCtrlConsulterDepartement from '../useCtrlConsulterDepartement';
+import ServiceDepartement from 'modele/rh/departement/ServiceDepartement';
+import { FormulaireConsultation, Texte, useRecupererParId } from 'waxant';
+import { ActionDepartement } from '../../ActionDepartement';
 
 const EtatDepartement = () => {
-    const { departement } = useCtrlConsulterDepartement();
+    const departement = useRecupererParId(
+        'idDepartement',
+        ServiceDepartement.recupererParId,
+        ActionDepartement.UcConsulterDepartement.RECUPERER_DEPARTEMENT_PAR_ID,
+    );
 
     if (!departement) {
         return null;
